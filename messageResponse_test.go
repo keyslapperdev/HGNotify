0af37673d07332090699cc66f29153d45b630264
name schema <bamf@thezspot.net>
2020-06-02 03:09:44 -0500

    Added test coverage for parsing the message to the bot
    Created messageResponse_test.go
      Subtests:
        TestParseArgs/Action_and_Group_args_are_parsed_and_returned
        TestParseArgs/Properly_identifies_as_from_admin
        TestParseArgs/Properly_finds_group_name_for_notify
        TestParseArgs/Properly_finds_group_name_for_notify/Notify_within_message
        TestParseArgs/Properly_finds_group_name_for_notify/Notify_in_front_of_message
        TestParseArgs/Properly_notes_self_when_provided

---
+++ messageResponse_test.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseArgs(t *testing.T) {
+	newMsgObj := messageResponse{
+		Message: message{
+			Sender: User{
+				Name: randString(10),
+				GID:  genUserGID(0),
+				Type: "HUMAN",
+			},
+		},
+		Room: space{
+			GID:  genRoomGID(0),
+			Type: "ROOM",
+		},
+	}
+
+	t.Run("Action and Group args are parsed and returned", func(t *testing.T) {
+		wantedGroupName := randString(10)
+		msgObj := newMsgObj
+		actions := []string{"create", "add", "remove", "disband", "restrict", "list", "syncgroup", "syncallgroups", "usage", "help"}
+
+		for _, wantedAction := range actions {
+			msgObj.Message.Text = BotName + " " + wantedAction + " " + wantedGroupName
+			args, msg, okay := msgObj.parseArgs()
+
+			if !okay {
+				t.Fatalf("Error parsing argument %q: %s", wantedAction, msg)
+			}
+
+			if args["action"] != wantedAction {
+				t.Fatalf("Action %q not returned", wantedAction)
+			}
+
+			if args["groupName"] != wantedGroupName {
+				t.Fatal("Group name not returned")
+			}
+		}
+	})
+
+	t.Run("Properly identifies as from admin", func(t *testing.T) {
+		msgObj := newMsgObj
+		MasterID = genUserGID(0)
+
+		msgObj.Room.Type = "DM"                 // Master only recognized via DM
+		msgObj.Message.Text = BotName + " list" // An action must be passed
+		msgObj.Message.Sender.GID = MasterID
+		msgObj.IsMaster = false
+
+		_, msg, okay := msgObj.parseArgs()
+
+		if !okay {
+			t.Fatalf("Something went wrong: %q", msg)
+		}
+
+		if !msgObj.IsMaster {
+			t.Fatal("Message object is not noted as from the admin")
+		}
+	})
+
+	t.Run("Properly finds group name for notify", func(t *testing.T) {
+		wantedGroupName := strings.ToLower(randString(10))
+		msgObj := newMsgObj
+
+		t.Run("Notify within message", func(t *testing.T) {
+			msgObj.Message.Text = "Some before text " + BotName + " " + wantedGroupName + " Some test text"
+
+			args, msg, okay := msgObj.parseArgs()
+
+			if !okay {
+				t.Fatalf("Something went wrong: %q", msg)
+			}
+
+			if args["action"] != "notify" || args["groupName"] != wantedGroupName {
+				t.Fatalf("Notify action not properly parsed\nObject Result: %+v", args)
+			}
+		})
+
+		t.Run("Notify in front of message", func(t *testing.T) {
+			Groups = make(GroupList)
+			Groups[wantedGroupName] = new(Group) //Group must exist for this form
+			msgObj.Message.Text = BotName + " " + wantedGroupName + " Some test text"
+
+			args, msg, okay := msgObj.parseArgs()
+
+			if !okay {
+				t.Fatalf("Something went wrong: %q", msg)
+			}
+
+			if args["action"] != "notify" || args["groupName"] != wantedGroupName {
+				t.Fatalf("Notify action not properly parsed\nObject Result: %+v", args)
+			}
+		})
+	})
+
+	t.Run("Properly notes self when provided", func(t *testing.T) {
+		msgObj := newMsgObj
+
+		msgObj.Message.Text = BotName + " add groupName Self"
+
+		args, msg, okay := msgObj.parseArgs()
+
+		if !okay {
+			t.Fatalf("Something went wrong: %q", msg)
+		}
+
+		if args["self"] == "" {
+			t.Fatal("Self not successfully returned")
+		}
+	})
+}
